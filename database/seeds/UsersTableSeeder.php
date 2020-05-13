81ad6794b1fdaaf3d6aadd97ac015b4ab360f261
xiaoke li <1001code@gmail.com>
2020-05-13 19:21:45 +0000

    fill user and topic data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        $avatar = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        $users = factory(\App\Models\User::class)
+                        ->times(10)
+                        ->make()
+                        ->each(function ($user, $index) use ($faker, $avatar){
+                            $user->avatar = $faker->randomElement($avatar);
+                        });
+
+        $user_array = $users->makeVisible(['password','remember_token'])->toArray();
+
+        \App\Models\User::insert($user_array);
+
+        $user = \App\Models\User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summer@example.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+
+    }
+}

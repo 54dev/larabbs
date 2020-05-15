b68557a651d020b9e431a0df2f040dc054618d20
xiaoke li <1001code@gmail.com>
2020-05-15 17:11:29 +0000

    locale

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Auth\AuthenticationException;
@@ -70,7 +69,7 @@ class AuthorizationsController extends Controller
         $credentials['password'] = $request->password;
 
         if(!$token = \Auth::guard('api')->attempt($credentials)){
-            throw new AuthenticationException('用户名或者密码错误');
+            throw new AuthenticationException(trans('auth.failed'));
         }
 
         return $this->responseWithToken($token)->setStatusCode(201);
--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Overtrue\EasySms\EasySms;

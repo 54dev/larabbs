03197e1a5f01d85cfc976650ea94063f2381e9b2
xiaoke li <1001code@gmail.com>
2020-05-15 15:49:34 +0000

    删除回复

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function(){
 
             // post reply
             Route::post('topics/{topic}/replies', 'RepliesController@store')->name('topics.replies.store');
+            // delete reply
+            Route::delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')->name('topics.replies.destroy');
         });
     });
 

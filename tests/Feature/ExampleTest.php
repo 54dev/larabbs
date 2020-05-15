05db2d1e1f0c4c33aa9fd1038f17fa05879101b7
xiaoke li <1001code@gmail.com>
2020-05-15 17:55:04 +0000

    phpunit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->json('GET', '/api/v1/topics/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertJsonFragment(['data','meta']);
     }
 }
